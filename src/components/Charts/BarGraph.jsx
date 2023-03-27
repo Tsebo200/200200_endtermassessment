0ac86eaffbcec1a2439ee9e278aac8712391af6f
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-03-27 10:43:59 +0200

    Craeted a Bar Graph
    Importing it to Map.jsx

---
+++ src/components/Charts/BarGraph.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {Bar} from 'react-chartjs-2';
+import '../Data/Data';
+import { Chart as ChartJS, LineController, LineElement, PointElement, LinearScale, Title, ArcElement, Tooltip, Legend, CategoryScale, BarElement } from 'chart.js';
+ChartJS.register(LineController, LineElement, PointElement, LinearScale, Title);
+
+
+function BarGraph() {
+
+
+  
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        BarElement,
+        Title,
+        Tooltip,
+        Legend
+      );
+
+
+  
+   const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+  
+  const labels = ['Jan', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [59, 80, 81, 56, 55, 40],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: [59, 80, 81, 56, 55, 40],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+  return (
+    <Bar options={options} data={data}/>
+  )
+}
+
+export default BarGraph
--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../Data/Data';
-import {Bar}
+
+
 function Maps() {
   return (
     <Link to="/"><p>Maps</p></Link>
