f874676e2f3db87f5509de510cd314fae16e76b3
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-03-27 12:00:06 +0200

    Starting with the styling
    Switched to sass from css

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { Button } from 'react-bootstrap';
+import '../styles/styles.sass';
 
 function Landing() {
 
@@ -25,16 +26,13 @@ function Landing() {
 // setRandomImage = <img src={{randomImage}} width="500px" height="500px"/>
 
   return (   
-    <>
-
+    <div className="background">
     <h1>Landing Page</h1>
     <Link to="/maps"><p>Landing</p></Link>
     {/* <div className="image-container">{randomImage}</div> */}
     <img src= "https://images.dog.ceo/breeds/pug/n02110958_13439.jpg" width="100px" height="100px" alt='image of dog'></img>
     <Button/>
-
-
-    </>
+    </div>
 
 
 
--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import BarGraph from './Charts/BarGraph';
 import LineGraph from './Charts/LineGraph';
 import BarGraphTwo from './Charts/BarGraphTwo';
+import '../styles/styles.sass'
 
 function Maps() {
   return (
     <>
-   
-    <Link to="/"><p>Maps</p></Link>
+   <center><h1>Maps Page</h1></center>
+    {/* <Link to="/"><p>Maps</p></Link>  */}     {/* This is the Link to */}
      <BarGraph/>
      <BarGraphTwo/>
      <LineGraph/>
